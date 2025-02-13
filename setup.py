ad630288cded3efc337e03b35c04e6e2fcf8a044
hardgames <95406644+hardgs@users.noreply.github.com>
2025-02-13 05:09:33 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "pyrubi",
-    version = "3.6.0",
-    author="Ali Ganji zadeh",
-    author_email = "ali.ganji.za@gmail.com",
+    name = "rubipy",
+    version = "0.0.1",
+    author="HardGames Group",
+    author_email = "test@gmail.com",
     description = "This is a powerful and easy library for building self bot in the Rubika.",
     keywords = ["rubika", "rubino", "pyrubi", "pyrubika", "bot", "chat"],
     long_description = open("README.md", encoding="utf-8").read(),
     python_requires="~=3.7",
     long_description_content_type = "text/markdown",
-    url = "https://github.com/AliGanji1/pyrubi",
+    url = "https://github.com/hardgs/rubipy",
     packages = find_packages(),
     install_requires = ["urllib3", "tqdm", "websocket-client", "pycryptodome", "mutagen", "filetype"],
     classifiers = [
